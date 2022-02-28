591378403c5d777fd5ded7de4b189c131c0e5191
go3212 <go3212@hotmail.com>
2022-02-28 12:43:00 +0100

    Ejercicio 1

---
+++ Bloque1/Sesion 1/Ejercicio 1/MyForm.h
@@ -0,0 +1,15 @@
+#ifndef __INTELLISENSE__ // code that generates an error squiggle 
+#include "ui_myform.h"
+#include <QSurfaceFormat>
+
+class MyForm : public QWidget
+{
+  	Q_OBJECT
+
+  	public:
+    	MyForm (QWidget* parent=0);
+  
+	private:
+		Ui::myform ui;
+};
+#endif
