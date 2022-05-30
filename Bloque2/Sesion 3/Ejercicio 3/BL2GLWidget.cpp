06ffb22819c90ef8e49b5bc38e9393fd18b7d502
go3212 <go3212@hotmail.com>
2022-05-30 14:54:59 +0200

    Ejercicio 3

---
+++ Bloque2/Sesion 3/Ejercicio 3/BL2GLWidget.cpp
@@ -0,0 +1,274 @@
+#include "BL2GLWidget.h"
+
+#include <iostream>
+
+BL2GLWidget::BL2GLWidget (QWidget* parent) : QOpenGLWidget(parent), program(NULL)
+{
+  setFocusPolicy(Qt::StrongFocus);  // per rebre events de teclat
+  homer.load("Patricio.obj");
+  homer_deg = 0.0f;
+
+  // angles
+  phi = theta = psi = 0.0f;
+  x_prev = y_prev = 0.0f;
+  zoom = 1.0f;
+}
+
+BL2GLWidget::~BL2GLWidget ()
+{
+  if (program != NULL)
+    delete program;
+}
+
+void BL2GLWidget::ini_camera ()
+{
+  OBS = boundingbox->get_center();
+  VRP = boundingbox->get_center();
+  up =  glm::vec3(0.0f, 1.0f, 0.0f);
+
+  // Ponemos el obs a 3*radio
+  OBS[2] += 3.0f*boundingbox->get_radius();
+
+  FOV = 2.0f*asin(1.0f/3.0f);
+  ra = (float)ample/(float)alt;
+  if (ra < 1.0f) // alto > ancho
+    FOV = 2.0f*atan((tan(FOV/2.0f)*(1.0f/ra)));
+  FOV *= zoom;
+  znear = 2.0f*boundingbox->get_radius();
+  zfar = 4.0f*boundingbox->get_radius();
+
+  boundingbox->print_data();
+
+  projectTransform();
+  viewTransform();
+}
+
+void BL2GLWidget::initializeGL ()
+{
+  // Cal inicialitzar l'ús de les funcions d'OpenGL
+  initializeOpenGLFunctions();  
+
+  glClearColor(0.5, 0.7, 1.0, 1.0); // defineix color de fons (d'esborrat)
+  carregaShaders();
+  creaBuffers();
+  escala = 1.0f;
+
+  glEnable(GL_DEPTH_TEST); 
+}
+
+void BL2GLWidget::projectTransform()
+{
+  glm::mat4 PM;
+  PM = glm::perspective(FOV, ra, znear, zfar);
+  glUniformMatrix4fv(PMLoc, 1, GL_FALSE, &PM[0][0]);
+}
+
+void BL2GLWidget::viewTransform()
+{
+  glm::mat4 VM(1.0f);
+  // Place OBS in origin.
+  VM = glm::translate(VM, glm::vec3(0.0f, 0.0f, -3.0f*boundingbox->get_radius()));
+  VM = glm::rotate(VM,       glm::radians(theta), glm::vec3(1.0f, 0.0f, 0.0f));
+  VM = glm::rotate(VM, -1.0f*glm::radians(psi),   glm::vec3(0.0f, 1.0f, 0.0f)); 
+  // Move VRP to origin.
+  VM = glm::translate(VM, -1.0f*VRP);
+  //VM = glm::lookAt(OBS, VRP, up);
+  glUniformMatrix4fv(VMLoc, 1, GL_FALSE, &VM[0][0]);
+}
+
+void BL2GLWidget::modelTransform (GLuint VAO) 
+{
+  // Matriu de transformació de model
+  glm::mat4 transform (1.0f);
+  transform = glm::translate(transform, boundingbox->get_center());
+  transform = glm::scale(transform, glm::vec3(escala));
+  transform = glm::translate(transform, glm::vec3(-1.0f, -1.0f, -1.0f)*boundingbox->get_center());
+  if (VAO == VAO_Homer)
+  {
+    transform = glm::rotate(transform, homer_deg, glm::vec3(0.0f, 1.0f, 0.0f));
+  }
+  if (VAO == VAO_suelo)
+    transform = glm::translate(transform, glm::vec3(0.0f, 0.0f, 0.0f));
+  glUniformMatrix4fv(transLoc, 1, GL_FALSE, &transform[0][0]);
+}
+
+void BL2GLWidget::paintGL () 
+{
+// En cas de voler canviar els paràmetres del viewport, descomenteu la crida següent i
+// useu els paràmetres que considereu (els que hi ha són els de per defecte)
+  glViewport (0, 0, ample, alt);
+  
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+  // Esborrem el frame-buffer
+  glClear (GL_COLOR_BUFFER_BIT);
+  // Carreguem la transformació de model
+  modelTransform(VAO_Homer);
+  // Activem el VAO per a pintar la caseta 
+  glBindVertexArray (VAO_Homer);
+  // pintem
+  glDrawArrays(GL_TRIANGLES, 0, homer.faces().size()*3);
+
+  // Carreguem la transformació de model
+  modelTransform(VAO_suelo);
+  // Activem el VAO per a pintar la caseta 
+  glBindVertexArray (VAO_suelo);
+  // pintem
+  glDrawArrays(GL_TRIANGLES, 0, 6);
+
+  glBindVertexArray (0);
+}
+
+
+void BL2GLWidget::resizeGL (int w, int h) 
+{
+// Aquest codi és necessari únicament per a MACs amb pantalla retina.
+#ifdef __APPLE__
+  GLint vp[4];
+  glGetIntegerv (GL_VIEWPORT, vp);
+  ample = vp[2];
+  alt = vp[3];
+#else
+  ample = w;
+  alt = h;
+  ini_camera ();
+
+#endif
+}
+
+void BL2GLWidget::keyPressEvent(QKeyEvent* event) 
+{
+  makeCurrent();
+  switch (event->key()) 
+  {
+    case Qt::Key_S: escala += 0.05;                 break;
+    case Qt::Key_D: escala -= 0.05;                 break;
+    case Qt::Key_R: homer_deg += float(M_PI)/4.0f;  break;
+    case Qt::Key_Z: zoom -= 0.01f;                  break;
+    case Qt::Key_X: zoom += 0.01f;                  break;
+    default: event->ignore(); break;
+  }
+  ini_camera ();
+  update();
+}
+
+void BL2GLWidget::mouseMoveEvent(QMouseEvent* event)
+{
+  makeCurrent();
+  
+  if (event->x() > x_prev) psi -= 1.0f;
+  if (event->x() < x_prev) psi += 1.0f;
+  if (event->y() > y_prev) theta += 1.0f;
+  if (event->y() < y_prev) theta -= 1.0f;
+
+  x_prev = event->x();
+  y_prev = event->y();
+
+  viewTransform();
+  update();
+}
+
+void BL2GLWidget::creaBuffers () 
+{
+  // Dades de la caseta
+  // Dos VBOs, un amb posició i l'altre amb color
+
+  
+  // Creació del Vertex Array Object per pintar
+  glGenVertexArrays(1, &VAO_Homer);
+  glBindVertexArray(VAO_Homer);
+
+  glGenBuffers(2, VBO_Homer);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO_Homer[0]);
+
+  BoundingBox* patbox = new BoundingBox();
+  patbox->insert_model(homer);
+  patbox->move(glm::vec3(-1.0f, -1.0f, -1.0f)*patbox->get_base_center());
+  patbox->uniform_set_height(4.0f);
+
+  // Homer es todo triangulos, cada triangulo tiene 3 vértices con 3 floats -> 9 floats, 
+  glBufferData(GL_ARRAY_BUFFER, 9*sizeof(float)*homer.faces().size(), patbox->get_vertexs(), GL_STATIC_DRAW);
+  glVertexAttribPointer(vertexLoc, 3, GL_FLOAT, false
+                        , 3*sizeof(float) // Tenemos que cada triplete de coords son 3 floats. 
+                        , 0);
+  glEnableVertexAttribArray(vertexLoc); 
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO_Homer[1]);
+  glBufferData(GL_ARRAY_BUFFER, 9*sizeof(float)*homer.faces().size(), homer.VBO_matdiff(), GL_STATIC_DRAW);
+  glVertexAttribPointer(colorLoc, 3, GL_FLOAT, false
+                        , 3*sizeof(float) // Tenemos que cada triplete de color son 3 floats. 
+                        , 0);
+  glEnableVertexAttribArray(colorLoc);      
+
+  glGenVertexArrays(1, &VAO_suelo);
+  glBindVertexArray(VAO_suelo);
+  glm::vec3 suelo[6] =
+  {
+    glm::vec3(2.5f, 0.0f, 2.5f),
+    glm::vec3(2.5f, 0.0f, -2.5f),
+    glm::vec3(-2.5f, 0.0f, 2.5f),
+
+    glm::vec3(-2.5f, 0.0f, -2.5f),
+    glm::vec3(2.5f, 0.0f, -2.5f),
+    glm::vec3(-2.5f, 0.0f, 2.5f)
+  };
+
+  glm::vec3 col_suelo[6] =
+  {
+    glm::vec3(0.0f, 0.0f, 0.0f),
+    glm::vec3(0.0f, 0.0f, 0.0f),
+    glm::vec3(0.0f, 0.0f, 0.0f),
+
+    glm::vec3(0.0f, 0.0f, 0.0f),
+    glm::vec3(0.0f, 0.0f, 0.0f),
+    glm::vec3(0.0f, 0.0f, 0.0f)
+
+  };
+
+  glGenBuffers(2, VBO_suelo);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO_suelo[0]);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(suelo), suelo, GL_STATIC_DRAW);
+  glVertexAttribPointer(vertexLoc, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), 0);
+  glEnableVertexAttribArray(vertexLoc);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO_suelo[1]);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(col_suelo), col_suelo, GL_STATIC_DRAW);
+  glVertexAttribPointer(colorLoc, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), 0);
+  glEnableVertexAttribArray(colorLoc);
+
+  // Buscamos la altura del homer.
+  boundingbox = patbox;
+  boundingbox->insert_model(suelo, 6);
+
+  glBindVertexArray (0);
+}
+
+void BL2GLWidget::carregaShaders()
+{
+  // Creem els shaders per al fragment shader i el vertex shader
+  QOpenGLShader fs (QOpenGLShader::Fragment, this);
+  QOpenGLShader vs (QOpenGLShader::Vertex, this);
+  // Carreguem el codi dels fitxers i els compilem
+  fs.compileSourceFile("shaders/basicShader.frag");
+  vs.compileSourceFile("shaders/basicShader.vert");
+  // Creem el program
+  program = new QOpenGLShaderProgram(this);
+  // Li afegim els shaders corresponents
+  program->addShader(&fs);
+  program->addShader(&vs);
+  // Linkem el program
+  program->link();
+  // Indiquem que aquest és el program que volem usar
+  program->bind();
+
+  // Obtenim identificador per a l'atribut “vertex” del vertex shader
+  vertexLoc = glGetAttribLocation (program->programId(), "vertex");
+  // Obtenim identificador per a l'atribut “color” del vertex shader
+  colorLoc = glGetAttribLocation (program->programId(), "color");
+  // Uniform locations
+  transLoc = glGetUniformLocation(program->programId(), "TG");
+  PMLoc = glGetUniformLocation(program->programId(), "PM");
+  VMLoc = glGetUniformLocation(program->programId(), "VM");
+}
+
